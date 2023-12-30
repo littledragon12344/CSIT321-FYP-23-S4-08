ce5a7d75c83274f1873d33fa0e72c49a007663a7
XDpartypooper <Roywuzzup@gmail.com>
2023-12-30 23:14:45 +0800

    Update ModelMaker.py

--- ModelMaker.py
+++ ModelMaker.py
@@ -1,23 +1,7 @@
-from mediapipe_model_maker import gesture_recognizer
+#from mediapipe_model_maker import gesture_recognizer
+
+#https://developers.google.com/mediapipe/solutions/vision/gesture_recognizer/python
+#https://www.youtube.com/watch?v=vODSFXEP-XY&t=134s
 
 
-# https://www.youtube.com/watch?v=yOP_FY2KTm8
-#Train a model (1)
-
-# Load the dataset
-data = gesture_recognizer.Dataset.from_folder(dirname='images')
-train_data, validation_data = data.split(0.8)
-
-#Train the custom model
-model = gesture_recognizer.GestureRecognizer.create(
-	train_data=train_data,
-	validation_data=validation_data,
-	hparams=gesture_recognizer.HParasms(export_dir=export_dir)
-)
-
-#Train a model (2)
-#evaluate using unseen data
-metric = model.evaluate(test_data)
-
-#export as model
-model.export_model(model_name='bruh.task')
+#Issues on making this work , so im just leaving a comment here
